6b334fc536ae755557b6cc54a10f0bfd1d6c6270
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-09-18 12:51:33 -0400

    Added ProductList component which fetch data directly from mock API

---
+++ src/components/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+  <dir>
+    <h1>Prodcut List</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">{{product.title}} - {{product.price}}</li>
+    </ul>
+  </dir>
+</template>
+
+<script>
+import shop from "@/api/shop";
+
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  created() {
+    shop.getProducts((products) => {
+      this.products = products;
+    });
+  },
+};
+</script>
+
+<style>
+</style>
